61431f4dae9d3ac60340661ee861d222749c1e8c
CasperNybroe <108397282+CasperNybroe@users.noreply.github.com>
2022-07-04 09:42:10 +0200

    Fixed typo

--- miner.py
+++ miner.py
@@ -175,6 +175,6 @@ def run():
             run()
         else:
             print("********* TERMINATING *****************")
-            print("Expection occurred 5 times. Terminating!")
+            print("Exception occurred 5 times. Terminating!")
 
 run()
